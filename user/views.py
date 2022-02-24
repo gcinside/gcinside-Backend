26a497a4743fa33a0c350c082e44caf70dd0e181
lot8229 <lot8229@kakao.com>
2022-02-24 18:18:43 +0900

    [delete] user app

--- user/views.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import viewsets # viewset import
-from .serializers import PersonSerializer # 생성한 serializer import
-from .models import User # Person model import
-
-class PersonViewSet(viewsets.ModelViewSet): # ModelViewSet 활용
-	queryset = Person.objects.all()
-    serializer_class = PersonSerializer
