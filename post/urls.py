d7b1620ae00eb33b21cd69056e0eccbda2cc75ec
Juhyunho <hyun050301@gmail.com>
2022-03-03 20:40:20 +0900

    Create Post App
    UploadPost

--- post/urls.py
+++ post/urls.py
@@ -5,5 +5,5 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('upload', UploadPostView.as_view(), name='post_upload'),
+    path('upload/', UploadPostView.as_view(), name='post_upload'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/views.py
+++ post/views.py
@@ -8,12 +8,12 @@ from gcinside.settings import DEFAULT_LOGGING
 from django.utils import timezone
 from django.contrib.auth import get_user
 from rest_framework.decorators import permission_classes
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 
 logging.config.dictConfig(DEFAULT_LOGGING)
 # Create your views here.
 class UploadPostView(APIView):
-    @permission_classes(AllowAny, )
+    @permission_classes(IsAuthenticated, )
     def post(self, request):
         if request.user.is_authenticated:
             serializer = PostSerializer(
@@ -32,4 +32,5 @@ class UploadPostView(APIView):
                 return JsonResponse({'message' : 'Post success'}, status=201)
             return JsonResponse({'message' : 'Bad request'}, status=400)
         else :
+            logging.info(request.user)
             return JsonResponse({'message' : 'auth error'}, status=401)
