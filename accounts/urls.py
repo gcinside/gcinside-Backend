07648b14ff56937d165d2812eb4ec7528ec372d9
lot8229 <lot8229@kakao.com>
2022-04-22 18:20:51 +0900

    github login urls add

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('google/callback/', views.google_callback, name='google_callback'),
     path('google/login/finish/', views.GoogleLogin.as_view(),
          name='google_login_todjango'),
+    path('github/login/', views.github_login, name='github_login'),
+    path('github/callback/', views.github_callback, name='github_callback'),
+    path('github/login/finish/', views.GithubLogin.as_view(), name='github_login_todjango'),
 ]
