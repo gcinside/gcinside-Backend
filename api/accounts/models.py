1aaa213c78b7b7ca10ad85a20f89b4971c535960
lot8229 <lot8229@kakao.com>
2022-06-10 07:58:04 +0900

    edit models.py & views.py apps.py syntax error edit

--- api/accounts/models.py
+++ api/accounts/models.py
@@ -1,14 +1,14 @@
-from django.db import models
-from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+from django.utils.translation import ngettext_lazy as _
+
 from .utils import random_name_generator
 
 
 class UserManager(BaseUserManager):
-
     def create_user(self, email, password, **extra_fields):
         if not email:
-            raise ValueError(_('The Email must be set'))
+            raise ValueError(_("The Email must be set"))
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -16,23 +16,27 @@ class UserManager(BaseUserManager):
         return user
 
     def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
 
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create_user(email, password, **extra_fields)
 
-class User(AbstractUser):
-    username = models.CharField(max_length=100)
-    email = models.EmailField(unique=True, max_length=255)
-    profile_image = models.ImageField(blank=True, null=True, upload_to='user_profile', default='user_profile/default.jpg')
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+class User(AbstractUser):
+    profile_image = models.ImageField(
+        blank=True,
+        null=True,
+        upload_to="user_profile",
+        default="user_profile/default.jpg",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
 
     objects = UserManager()
 
@@ -46,11 +50,12 @@ class User(AbstractUser):
                 self.username = random_name_generator()
         super(User, self).save()
 
+
 class ReportUser(models.Model):
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='report')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="report")
     reporter = models.ForeignKey(User, on_delete=models.CASCADE)
     reason = models.TextField()
     reported_at = models.DateTimeField()
 
     def __str__(self) -> str:
-        return 'report ' + self.user.username
+        return "report " + self.user.username
